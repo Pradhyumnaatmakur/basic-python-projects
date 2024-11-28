df5a21ff7c8bee62edc4dab06b226d4072907e62
pradhyumna <pradhyumnaatmakur1@gmail.com>
2024-11-28 21:07:14 +0530

    madlib

---
+++ madlib.py
@@ -0,0 +1,28 @@
+adjective = input("Enter adjective: ")
+name = input("Enter name of your friend: ")
+animal = input("Enter your favorite animal: ")
+verb = input("Enter verb: ")
+verb_ending_with_ing = input("Enter a verb ending with -ing: ")
+plural_noun = input("Enter plural noun: ")
+noun = input("Enter noun: ")
+
+madlib = f"""One day, my best friend {name} and I decided 
+           to visit the zoo. 
+           We were so excited to see the {adjective} animals! 
+           When we arrived, the first thing we saw was a {adjective} 
+           {animal} 
+           jumping up and down in its cage.
+           Next, we watched a {adjective} {animal} that loved to {verb}. 
+           It was so funny that we couldn’t {verb_ending_with_ing}.
+           We also saw a {adjective} zookeeper 
+           feeding the animals {plural_noun}. 
+           I couldn’t believe it when one of the animals 
+           escaped and started 
+           {verb_ending_with_ing} toward us!
+           By the end of the day, we were {adjective} 
+           but had so much fun. 
+           We even bought a {noun} as a souvenir. 
+           It was truly the {adjective} day ever!"""
+
+print(madlib)
+           
